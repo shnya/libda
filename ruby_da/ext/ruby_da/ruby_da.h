8fc337ea467795b4b6400754c00dbaec71a15fdb
Masahiko Higashiyama <higashiyama@scaleout.jp>
2015-03-31 15:46:57 +0900

    changed repository structure.

--- ruby_da/ext/ruby_da/ruby_da.h
+++
@@ -1,6 +0,0 @@
-#ifndef RUBY_DA_H
-#define RUBY_DA_H 1
-
-#include "ruby.h"
-
-#endif /* RUBY_DA_H */
--- ruby_da/test/minitest_helper.rb
+++
@@ -1,4 +0,0 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'ruby_da'
-
-require 'minitest/autorun'
